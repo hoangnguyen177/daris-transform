7c3069c57e5c5a01eaa971e0f538a97740f87a30
Wilson Liu <wliu5@unimelb.edu.au>
2015-05-30 23:50:02 +1000

    corrected the old package name.

--- src/main/mfpkg/old-release-cleanup.tcl
+++ src/main/mfpkg/old-release-cleanup.tcl
@@ -1,7 +1,7 @@
 # remove the predeccessor (old release): Transform
-if { [xvalue exists [package.exists :package Transform]] == "true" } {
-    if { [xvalue package/vendorurl [package.describe :package Transform]] == "http://www.neuroimaging.org.au" } {
-        package.uninstall :package Transform
+if { [xvalue exists [package.exists :package "Transform Framework" ]] == "true" } {
+    if { [xvalue package/vendorurl [package.describe :package "Transform Framework" ]] == "http://www.neuroimaging.org.au" } {
+        package.uninstall :package "Transform Framework"
     }
 }
 if { [xvalue exists [asset.exists :id path=/mflux/plugins/transform-plugin.jar]] == "true" } {
